1351a98721a4688967937ff78a2dd4eec5e9ca8d
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2016-06-30 00:56:05 +0200

    Add MDL assets into client project
    - assets
    - example markup

--- StorylineTracker/src/StorylineTracker.Client/src/app/app.component.ts
+++ StorylineTracker/src/StorylineTracker.Client/src/app/app.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
-
+import { Component, AfterViewInit } from '@angular/core';
+declare var componentHandler: any;
 @Component({
   moduleId: module.id,
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements AfterViewInit  {
+  title = 'Storyline Tracker';
+
+  ngAfterViewInit() {
+    componentHandler.upgradeDom();
+  }
 }
