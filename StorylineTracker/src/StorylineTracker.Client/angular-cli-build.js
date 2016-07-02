879858d9dca5b16527eb9e2d19f3cd0e2267ef4b
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2016-07-02 19:05:28 +0200

    Move MD Icons to NPM managed vendor folder
    - update CLI config
    - remove old assets
    - update public folder assets

--- StorylineTracker/src/StorylineTracker.Client/angular-cli-build.js
+++ StorylineTracker/src/StorylineTracker.Client/angular-cli-build.js
@@ -16,7 +16,8 @@ module.exports = function(defaults) {
       'reflect-metadata/**/*.+(ts|js|js.map)',
       'rxjs/**/*.+(js|js.map)',
       '@angular/**/*.+(js|js.map)',
-      'material-design-lite/dist/material.+(css|min.css|css.map|js|js.map)'
+      'material-design-lite/dist/material.+(css|min.css|css.map|js|js.map)',
+      'material-design-icons/iconfont/**'
     ]
   });
 };
