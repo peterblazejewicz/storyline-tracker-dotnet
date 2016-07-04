525a9d16a8f2be9f27a62e97313d6d69b86dbcf4
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2016-07-04 23:52:08 +0200

    Fix modal service test specs

--- StorylineTracker/src/StorylineTracker.Client/src/app/modal/modal.component.spec.ts
+++ StorylineTracker/src/StorylineTracker.Client/src/app/modal/modal.component.spec.ts
@@ -11,14 +11,15 @@ import {
 } from '@angular/core/testing';
 
 import { ModalComponent } from './modal.component';
+import { ModalService } from './modal.service';
 
-beforeEachProviders(() => [ElementRef]);
+beforeEachProviders(() => [ElementRef, ModalService]);
 
 describe('Component: Modal', () => {
 
   it('should create an instance',
-  inject([ElementRef], (dialog: ElementRef) => {
-    let component = new ModalComponent(dialog);
+  inject([ElementRef, ModalService], (dialog: ElementRef, service: ModalService) => {
+    let component = new ModalComponent(dialog, service);
     expect(component).toBeTruthy();
   }));
 });
